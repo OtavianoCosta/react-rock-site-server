d8bc3f886a835725e29ab39f089af72bb12cfb84
Gabriel Otaviano <gabriel.oc1@hotmail.com>
2023-10-11 23:23:06 -0300

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const rotaCamiseta = require("./Rotas/camisetas")
+const cors = require("cors")
+
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/camisetas', rotaCamiseta)
 
 const port = 8000
 
 app.listen(port, () => {
-    console.log('Escutando a porta ${port}')
+    console.log(`Escutando a porta ${port}`)
 })
